030f259f655e0181bc94f6dcec90c4fd832efdb9
SemyonKuznetsovNSPK <92925990+SemyonKuznetsovNSPK@users.noreply.github.com>
2024-10-07 17:38:27 +0300

    Update Package.swift Fixed GRDB deps for XCode 16

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/roxiemobile-forks/GRDB.swift",
-            exact: "5.26.1-patch.1"
+            exact: "5.26.1-patch.2"
         ),
         .package(
             url: "https://github.com/roxiemobile/swift-commons-ios",
