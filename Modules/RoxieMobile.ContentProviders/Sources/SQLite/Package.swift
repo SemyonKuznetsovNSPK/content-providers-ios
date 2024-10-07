178563f0df7ad7262314f3095b12355d481107de
SemyonKuznetsovNSPK <92925990+SemyonKuznetsovNSPK@users.noreply.github.com>
2024-10-07 18:08:21 +0300

    Update Package.swift

--- Modules/RoxieMobile.ContentProviders/Sources/SQLite/Package.swift
+++ Modules/RoxieMobile.ContentProviders/Sources/SQLite/Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .package(path: "FileSystem"),
         .package(
             url: "https://github.com/SemyonKuznetsovNSPK/GRDB.swift",
-            exact: "5.26.1-patch.2"
+            exact: "5.26.1-patch.3"
         ),
         .package(
             url: "https://github.com/weichsel/ZIPFoundation",
